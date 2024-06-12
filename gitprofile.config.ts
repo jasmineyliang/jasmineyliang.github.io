e0fb9440deeec2b0a7d1052611a8f50865e940ef
Jasmine Liang <150869870+jasmineyliang@users.noreply.github.com>
2024-06-12 17:33:41 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -108,12 +108,12 @@ const CONFIG = {
     },
   ],
   certifications: [
-    // {
-    //   name: 'Lorem ipsum',
-    //   body: 'Lorem ipsum dolor sit amet',
-    //   year: 'March 2022',
-    //   link: 'https://example.com',
-    // },
+    {
+      name: 'Stanford University - Machine Learning ',
+      body: 'Lorem ipsum dolor sit ametNumPy & scikit-learn: Build and train supervised models for prediction and binary classification, such as linear and logistic regression. TensorFlow: Construct and train neural networks for multi-class classification. Implement decision trees and ensemble methods. Apply ML development and unsupervised learning techniques, e.g. clustering and anomaly detection. Recommender Systems: Develop systems using collaborative filtering, content-based deep learning, and deep reinforcement learning models.',
+      year: '2024',
+      link: 'https://www.linkedin.com/in/jypliang/details/certifications/1718230005331/single-media-viewer/?profileId=ACoAADgFQNQB1HOjJMI8bN1EdeEX-tbbWl4mEoI',
+    },
   ],
   educations: [
     {
